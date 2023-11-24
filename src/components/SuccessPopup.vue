f533772e19b97a98b7c50af1675fd58cb42aeac9
Vinicius Oliveira <vinicius9141@gmail.com>
2023-11-24 13:49:42 -0300

    pop up new style

--- src/components/SuccessPopup.vue
+++ src/components/SuccessPopup.vue
@@ -32,7 +32,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(47, 221, 12, 0.7);
   display: flex;
   justify-content: center;
   align-items: center;
