464c7a48f1df2cc193c997103c81c139a92eb743
Vinicius Henrique <vinicius9141@gmail.com>
2023-11-23 23:38:25 -0300

    redirecto to instagram

--- src/components/StoreList.vue
+++ src/components/StoreList.vue
@@ -1,96 +1,95 @@
-  <template>
-    <div>
-      <h2 class="store-list-title">Todas as Lojas</h2>
-      
+<template>
+  <div>
+    <h2 class="store-list-title">Todas as Lojas</h2>
 
-      
-      <div class="category-filter-wrapper">
+    <div class="category-filter-wrapper">
       <!-- Campo de Busca -->
-        <div class="search-filter">
-          <label for="searchInput">Buscar Loja:</label>
-          <div class="search-wrapper">
+      <div class="search-filter">
+        <label for="searchInput">Buscar Loja:</label>
+        <div class="search-wrapper">
           <input id="searchInput" v-model="searchQuery" placeholder="Digite para buscar..." />
           <i class="fas fa-search"></i>
-
-          </div>
-      </div>
-
-        <!-- Filtro de Categorias -->
-        <div class="categoryFilter">
-          <label for="categorySelect">Filtrar por Categoria:</label>
-            <select id="categorySelect" v-model="selectedCategory">
-            <option value="">Todas</option>
-            <option v-for="category in categories" :key="category">{{ category }}</option>
-          </select>
         </div>
       </div>
 
-
-
-      <ul class="store-list">
-        <li v-for="loja in filteredLojas" :key="loja.id" class="store-item">
-          <h3 class="store-title">{{ loja.data().Nome }}</h3>
-          
-          <p class="store-detail"><i class="fa-brands fa-instagram"></i>Instagram:
-            
-             <a :href="loja.data().InstagramLink">{{ loja.data().InstagramLink}}</a>
-            </p>
-          <img :src="loja.data().Imagem" alt="Imagem da Loja" class="store-image" />
-          <p class="store-detail">Categoria: {{ loja.data().Categoria }}</p>
-        </li>
-      </ul>
+      <!-- Filtro de Categorias -->
+      <div class="categoryFilter">
+        <label for="categorySelect">Filtrar por Categoria:</label>
+        <select id="categorySelect" v-model="selectedCategory">
+          <option value="">Todas</option>
+          <option v-for="category in categories" :key="category">{{ category }}</option>
+        </select>
+      </div>
     </div>
-  </template>
 
+    <ul class="store-list">
+      <li v-for="loja in filteredLojas" :key="loja.id" class="store-item">
+        <h3 class="store-title">{{ loja.data().Nome }}</h3>
 
-  <script>
-  import { getFirestore, collection, getDocs } from 'firebase/firestore';
+        <p class="store-detail">
+          <i class="fa-brands fa-instagram"></i>Instagram:
+          <a :href="getInstagramLink(loja.data().InstagramLink)" target="_blank">{{ loja.data().InstagramLink }}</a>
+        </p>
+        <img :src="loja.data().Imagem" alt="Imagem da Loja" class="store-image" />
+        <p class="store-detail">Categoria: {{ loja.data().Categoria }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
 
-  export default {
-    data() {
-      return {
-        lojas: [],
-        selectedCategory: '', // Categoria selecionada no filtro
-        searchQuery: '', // Consulta de busca
-      };
+<script>
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+export default {
+  data() {
+    return {
+      lojas: [],
+      selectedCategory: '', // Categoria selecionada no filtro
+      searchQuery: '', // Consulta de busca
+    };
+  },
+  async created() {
+    const db = getFirestore(this.$firebaseApp);
+    const lojasCollection = collection(db, 'lojas');
+
+    try {
+      const querySnapshot = await getDocs(lojasCollection);
+
+      querySnapshot.forEach((doc) => {
+        this.lojas.push(doc);
+      });
+    } catch (error) {
+      console.error('Erro ao obter as lojas:', error);
+    }
+  },
+  computed: {
+    categories() {
+      // Obtenha todas as categorias únicas de suas lojas
+      const uniqueCategories = [...new Set(this.lojas.map((loja) => loja.data().Categoria))];
+      return [''].concat(uniqueCategories); // Adicione uma opção para "Todas" as categorias
     },
-    async created() {
-      const db = getFirestore(this.$firebaseApp);
-      const lojasCollection = collection(db, 'lojas');
+    filteredLojas() {
+      // Filtre as lojas com base na categoria selecionada e na consulta de busca
+      return this.lojas.filter((loja) => {
+        const matchesCategory = !this.selectedCategory || loja.data().Categoria === this.selectedCategory;
+        const matchesSearch = !this.searchQuery || loja.data().Nome.toLowerCase().includes(this.searchQuery.toLowerCase());
+        return matchesCategory && matchesSearch;
+      });
+    },
+  },
+  methods: {
+    getLojasByCategory(category) {
+      // Filtra as lojas para uma categoria específica
+      return this.lojas.filter((loja) => loja.data().Categoria === category);
+    },
+    getInstagramLink(username) {
+      // Gere o link do Instagram redirecionando para o perfil do usuário
+      return `https://www.instagram.com/${username}`;
+    },
+  },
+};
+</script>
 
-      try {
-        const querySnapshot = await getDocs(lojasCollection);
-
-        querySnapshot.forEach((doc) => {
-          this.lojas.push(doc);
-        });
-      } catch (error) {
-        console.error('Erro ao obter as lojas:', error);
-      }
-    },
-    computed: {
-      categories() {
-        // Obtenha todas as categorias únicas de suas lojas
-        const uniqueCategories = [...new Set(this.lojas.map((loja) => loja.data().Categoria))];
-        return [''].concat(uniqueCategories); // Adicione uma opção para "Todas" as categorias
-      },
-      filteredLojas() {
-        // Filtre as lojas com base na categoria selecionada e na consulta de busca
-        return this.lojas.filter((loja) => {
-          const matchesCategory = !this.selectedCategory || loja.data().Categoria === this.selectedCategory;
-          const matchesSearch = !this.searchQuery || loja.data().Nome.toLowerCase().includes(this.searchQuery.toLowerCase());
-          return matchesCategory && matchesSearch;
-        });
-      },
-    },
-    methods: {
-      getLojasByCategory(category) {
-        // Filtra as lojas para uma categoria específica
-        return this.lojas.filter((loja) => loja.data().Categoria === category);
-      },
-    },
-  };
-  </script>
 
 
   <style scoped>
